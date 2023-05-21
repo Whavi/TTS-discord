6cd70744099bf7549ce01f183ea5afb8f0d921b9
B.Fethi <54514676+Whavi@users.noreply.github.com>
2023-05-21 03:09:55 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+
 import asyncio
 import discord
+import os
 from discord.ext import commands
 from discord import FFmpegPCMAudio
 from gtts import gTTS
-import os
 from dotenv import load_dotenv
 
 # Charge les variables d'environnement à partir du fichier .env
@@ -14,24 +15,32 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 # Crée le bot avec le préfixe "!"
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+intents = discord.Intents.all()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+intents.guilds = True
+intents.members = True
 
 # Affiche un message dans la console quand le bot est prêt
 
 
 @bot.event
 async def on_ready():
-    print(f'Le bot {bot.user} est prêt !')
+    print(f'Le bot {bot.user.name} est prêt !')
+    print(f"ID : {bot.user.id}")
 
-# Déconnecte le bot lorsqu'il n'y a plus personne dans le salon un certain temps
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            if filename[:-3] not in ["view"]:
+                await bot.load_extension(f"cogs.{filename[:-3]}")
+                print(f"le cog {filename} est bien charger !")
 
-
-@bot.event
-async def on_voice_state_update():
-    if bot.voice_clients and bot.voice_clients[0].channel:
-        channel = bot.voice_clients[0].channel
-        if not channel.members:  # Aucun utilisateur dans le canal vocal
-            await bot.voice_clients[0].disconnect()
+    try:
+        synced = await bot.tree.sync()
+        print(f"syncro {len(synced)} commands")
+    except Exception as e:
+        print(e)
 
 
 # Commande pour rejoindre le salon vocal de l'utilisateur
@@ -39,6 +48,7 @@ async def on_voice_state_update():
 
 @bot.command(name='join')
 async def join(ctx):
+    await ctx.message.delete()
     if ctx.author.voice is None:
         await ctx.send("Tu n'es pas connecté à un salon vocal.")
         return
@@ -51,13 +61,14 @@ async def join(ctx):
         await ctx.voice_client.move_to(voice_channel)
     print(
         f"Connecté au salon vocal {voice_channel.name} dans le serveur de {server_name} !")
-    await ctx.message.delete()
+
 
 # Commande pour quitter le salon vocal de l'utilisateur
 
 
 @bot.command(name='leave')
 async def leave(ctx):
+    await ctx.message.delete()
     server_name = ctx.guild.name
     voice_channel = ctx.author.voice.channel
     voice_client = ctx.voice_client
@@ -66,7 +77,7 @@ async def leave(ctx):
             voice_client.stop()
         await voice_client.disconnect()
         await print(f"Déconnecté du salon vocal {voice_channel.name} dans le serveur de {server_name}.")
-        await ctx.message.delete()
+
     else:
         await ctx.send("Je ne suis pas connecté à un salon vocal.")
 
@@ -75,6 +86,7 @@ async def leave(ctx):
 
 @bot.command()
 async def tts(ctx, *args: str):
+    await ctx.message.delete()
     text = " ".join(args)
     user = ctx.message.author
     if user.voice != None:
@@ -88,7 +100,6 @@ async def tts(ctx, *args: str):
         sound.save("audio.wav")
         source = FFmpegPCMAudio('audio.wav')
         vc.play(source)
-        await ctx.message.delete()
 
     else:
         await ctx.send("Le bot n'est pas connecter dans votre salon")
@@ -98,6 +109,7 @@ async def tts(ctx, *args: str):
 
 @bot.command()
 async def replay(ctx):
+    await ctx.message.delete()
     user = ctx.message.author
     if user.voice is not None:
         try:
@@ -106,9 +118,33 @@ async def replay(ctx):
             vc = ctx.voice_client
             source = FFmpegPCMAudio('audio.wav')
             vc.play(source)
-        await ctx.message.delete()
     else:
         await ctx.send("Le bot n'est pas connecter dans votre salon")
+
+
+@bot.command()
+async def disconnect(ctx, member: discord.Member):
+    # Vérifie si le membre spécifié est dans un canal vocal
+    if member.voice and member.voice.channel:
+        # Déconnecte le membre du canal vocal
+        await member.voice.channel.move_to(None)
+        await ctx.send(f"Le membre **__{member.display_name}__** a été déconnecté.")
+    else:
+        await ctx.send(f"Le membre {member.display_name} n'est pas connecté à un canal vocal.")
+
+
+@bot.command()
+async def clear(ctx, nombre: int):
+    if not ctx.author.guild_permissions.manage_messages:
+        await ctx.message.delete()
+        await ctx.send("Vous n'avez pas les permissions pour supprimer les messages")
+        await asyncio.sleep(3)
+        await ctx.channel.purge(limit=1)
+        return
+
+    async for message in ctx.channel.history(limit=nombre):
+        await message.delete()
+        await asyncio.sleep(1)
 
 # Lance le bot
 bot.run(TOKEN)
